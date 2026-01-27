65782d22c39b7f87013d2000d2e64b3477b82428
codefixer2 <tushargkd230@gmail.com>
2026-01-27 22:03:42 +0530

    updated api v1

--- backend/app/api/v1/endpoints/__init__.py
+++ backend/app/api/v1/endpoints/__init__.py
@@ -6,3 +6,10 @@ api_v1_router = APIRouter()
 
 #include each end point router 
 api_v1_router.include_router(analytics.router,prefix="/anylytics", tags=["Analytics"])
+api_v1_router.include_router(auth.router, prefix="/auth", tags=["Authentication"])
+api_v1_router.include_router(crops.router,prefix="/crops", tags=["Crops"])
+api_v1_router.include_router(farms.router, prefix="/farms", tags=["crops"])
+api_v1_router.include_router(health.router, prefix="/health", tags=["health"])
+api_v1_router.include_router(predictions.router, prefix="/predictions", tags= ["predictions"])
+api_v1_router.include_router(users.router, prefix="/users", tags= ["Users"])
+api_v1_router.include_router(weather.router, prefix="/prefix", tags=["weather"])
---
+++ backend/app/api/v1/endpoints/analytics.py
@@ -0,0 +1,2 @@
+#app/api/api_v1/endpoints/anylytics.py
+from fastapi import APIRouter, Depends , Httpexception
